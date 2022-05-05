1309f834aa728b3c2788a7265825a16094428284
Jason A. Donenfeld <Jason@zx2c4.com>
2022-05-05 02:20:22 +0200

    Revert "random: handle latent entropy and command line from random_init()"
    This reverts commit b16b1b66b7d47e09813a033ce52e954afeb505dd.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -882,13 +882,12 @@ early_param("random.trust_bootloader", parse_trust_bootloader);
 
 /*
  * The first collection of entropy occurs at system boot while interrupts
- * are still turned off. Here we push in latent entropy, RDSEED, a timestamp,
- * utsname(), and the command line. Depending on the above configuration knob,
- * RDSEED may be considered sufficient for initialization. Note that much
- * earlier setup may already have pushed entropy into the input pool by the
- * time we get here.
+ * are still turned off. Here we push in RDSEED, a timestamp, and utsname().
+ * Depending on the above configuration knob, RDSEED may be considered
+ * sufficient for initialization. Note that much earlier setup may already
+ * have pushed entropy into the input pool by the time we get here.
  */
-int __init random_init(const char *command_line)
+int __init rand_initialize(void)
 {
 	size_t i;
 	ktime_t now = ktime_get_real();
@@ -910,8 +909,6 @@ int __init random_init(const char *command_line)
 	}
 	_mix_pool_bytes(&now, sizeof(now));
 	_mix_pool_bytes(utsname(), sizeof(*(utsname())));
-	_mix_pool_bytes(command_line, strlen(command_line));
-	add_latent_entropy();
 
 	if (crng_ready())
 		crng_reseed();
