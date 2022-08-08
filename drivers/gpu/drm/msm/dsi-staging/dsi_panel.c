1f3ba18a5b7d8eba1713fa8bd7dfd591db006b72
Ivan Vecera <ivan@cera.cz>
2022-08-08 18:37:29 +0200

    drm/msm/dsi-staging: Read and parse extra HBM FOD commands from device tree
    Read extra HBM FOD commands that are used to go from HBM mode
    enabled by FOD touch back to one of DOZE mode.
    
    Commands:
    HBM_FOD_OFF_DOZE_LBM_ON
    - disable HBM and enter LBM doze mode when user releases FOD sensor
    HBM_FOD_OFF_DOZE_HBM_ON
    - should disable HBM and enter HBM doze mode when user releases FOD sensor
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I86855f7882057939a6e2d73210494acb25f0d8e7

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1941,6 +1941,8 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dimming-brightness-command",
 	"qcom,mdss-dsi-brightness-command",
 	"qcom,mdss-dsi-dimming-enable-command",
+	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-hbm-on-command",
+	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-lbm-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command",
 };
@@ -1979,6 +1981,8 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dimming-brightness-command-state",
 	"qcom,mdss-dsi-brightness-command-state",
 	"qcom,mdss-dsi-dimming-enable-command-state",
+	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-hbm-on-command-state",
+	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-lbm-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command-state",
 };
