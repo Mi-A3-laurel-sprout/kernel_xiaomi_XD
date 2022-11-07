473e03c95baff02f0cd3e3d2621ac52babc61eba
Magicxavi <rolfpeterkesche@gmail.com>
2022-11-07 06:14:18 +0200

    fixup! "cpuidle: Optimize pm_qos notifier callback and IPI semantics."
    The pm_qos callback currently suffers from a number of pitfalls: it
    sends IPIs to CPUs that may not be idle, waits for those IPIs to finish
    propagating while preemption is disabled (resulting in a long busy wait
    for the pm_qos_update_target() caller), and needlessly calls a no-op
    function when the IPIs are processed.
    
    Optimize the pm_qos notifier by only sending IPIs to CPUs that are
    idle, and by using arch_send_wakeup_ipi_mask() instead of
    smp_call_function_many(). Using IPI_WAKEUP instead of IPI_CALL_FUNC,
    which is what smp_call_function_many() uses behind the scenes, has the
    benefit of doing zero work upon receipt of the IPI; IPI_WAKEUP is
    designed purely for sending an IPI without a payload, whereas
    IPI_CALL_FUNC does unwanted extra work just to run the empty
    smp_callback() function.
    
    Determining which CPUs are idle is done efficiently with an atomic
    bitmask instead of using the wake_up_if_idle() API, which checks the
    CPU's runqueue in an RCU read-side critical section and under a spin
    lock. Not very efficient in comparison to a simple, atomic bitwise
    operation. A cpumask isn't needed for this because NR_CPUS is
    guaranteed to fit within a word.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ie00b0c7bfbad060be66cbd66c4884713d3fc0423

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -238,7 +238,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_start = ns_to_ktime(local_clock());
 
 	stop_critical_timings();
+	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
+	cpuidle_clear_idle_cpu(dev->cpu);
 	start_critical_timings();
 
 	sched_clock_idle_wakeup_event();
@@ -659,17 +661,6 @@ int cpuidle_register(struct cpuidle_driver *drv,
 EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
-
-static void wake_up_idle_cpus(void *v)
-{
-	unsigned long cpus = atomic_read(&idled) & *cpumask_bits(to_cpumask(v));
-
-	/* Use READ_ONCE to get the isolated mask outside cpu_add_remove_lock */
-	cpus &= ~READ_ONCE(*cpumask_bits(cpu_isolated_mask));
-	if (cpus)
-		arch_send_wakeup_ipi_mask(to_cpumask(&cpus));
-}
-
 /*
  * This function gets called when a part of the kernel has a new latency
  * requirement.  This means we need to get only those processors out of their
@@ -679,7 +670,13 @@ static void wake_up_idle_cpus(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_idle_cpus(v);
+	unsigned long cpus = atomic_read(&idled) & *cpumask_bits(to_cpumask(v));
+
+	/* Use READ_ONCE to get the isolated mask outside cpu_add_remove_lock */
+	cpus &= ~READ_ONCE(*cpumask_bits(cpu_isolated_mask));
+	if (cpus)
+		arch_send_wakeup_ipi_mask(to_cpumask(&cpus));
+
 	return NOTIFY_OK;
 }
 
