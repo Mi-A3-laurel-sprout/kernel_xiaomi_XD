9715c8d3819baf049048965e5ce799da1f08eec3
Magicxavi <rolfpeterkesche@gmail.com>
2022-11-16 11:47:50 +0200

    build.sh: correct the zip name
    clean the code a bit in proper way

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ default='\033[0m'
 CLANG_DIR=$HOME/drive2/syb13/prebuilts/clang/host/linux-x86/clang-r450784d
 KERNEL_DIR=$PWD
 Anykernel_DIR=$KERNEL_DIR/AnyKernel3/
-DATE=$(date +"-%d%m%Y")
-TIME=$(date +"-%H.%M.%S")
+DATE=$(date +"[%d%m%Y]")
+TIME=$(date +"%H.%M.%S")
 KERNEL_NAME="NoName"
-DEVICE="-laurel_sprout-"
+DEVICE="laurel_sprout"
 ANDROID_VER="[a12+a13]"
-FINAL_ZIP="$KERNEL_NAME""$ANDROID_VER""$DEVICE""$DATE"
+FINAL_ZIP="$KERNEL_NAME"-"$DEVICE"-"$ANDROID_VER"-"$DATE"
 
 BUILD_START=$(date +"%s")
 
