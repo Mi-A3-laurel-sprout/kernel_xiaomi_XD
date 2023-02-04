0f61ef08143d1f86fa08347d9d35cf397415cd53
Magicxavi <rolfpeterkesche@gmail.com>
2023-02-04 03:09:49 +0200

    fs: add kernelsu support

--- fs/exec.c
+++ fs/exec.c
@@ -1712,7 +1712,8 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 	return ret;
 }
-
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1721,6 +1722,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
