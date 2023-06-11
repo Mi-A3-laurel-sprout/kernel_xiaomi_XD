fd1d25283e64e492544fb1789f5e5f8c87ec14a1
Magicxavi <rolfpeterkesche@gmail.com>
2023-06-12 02:06:33 +0300

    drm: msm: expose fod hbm node

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -48,6 +48,7 @@
 #define MAX_NAME_SIZE	64
 
 #define DSI_CLOCK_BITRATE_RADIX 10
+#define DSI_FOD_HBM_RADIX 10
 #define MAX_TE_SOURCE_ID  2
 #define AOD_BRIGHTNESS 190
 
@@ -5466,6 +5467,65 @@ static struct attribute_group dynamic_dsi_clock_fs_attrs_group = {
 	.attrs = dynamic_dsi_clock_fs_attrs,
 };
 
+static ssize_t sysfs_fod_hbm_read(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display;
+	struct dsi_panel *panel;
+	int rc = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return -EINVAL;
+	}
+
+	panel = display->panel;
+
+	rc = snprintf(buf, PAGE_SIZE, "%d\n", panel->fod_hbm_enabled);
+
+	return rc;
+}
+
+static ssize_t sysfs_fod_hbm_write(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsi_display *display;
+	struct dsi_panel *panel;
+	int status;
+	int rc = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return -EINVAL;
+	}
+
+	rc = kstrtoint(buf, DSI_FOD_HBM_RADIX, &status);
+	if (rc) {
+		pr_err("%s: kstrtoint failed. rc=%d\n", __func__, rc);
+		return rc;
+	}
+
+	panel = display->panel;
+
+	dsi_panel_set_fod_hbm(panel, !!status);
+
+	return count;
+}
+
+static DEVICE_ATTR(fod_hbm, 0644,
+			sysfs_fod_hbm_read,
+			sysfs_fod_hbm_write);
+
+static struct attribute *fod_hbm_fs_attrs[] = {
+	&dev_attr_fod_hbm.attr,
+	NULL,
+};
+static struct attribute_group fod_hbm_fs_attrs_group = {
+	.attrs = fod_hbm_fs_attrs,
+};
+
 static ssize_t sysfs_fod_ui_read(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -5564,6 +5624,11 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 {
 	int rc = 0;
 	struct device *dev = &display->pdev->dev;
+
+	rc = sysfs_create_group(&dev->kobj,
+			&fod_hbm_fs_attrs_group);
+	if (rc)
+		pr_err("failed to create fod hbm device attributes");
 
 	rc = sysfs_create_group(&dev->kobj, &display_fs_attrs_group);
 	if (rc)
