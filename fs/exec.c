8ff9b4d8eb39276fe01ad800e4bd4b2ae1fc7256
Magicxavi <rolfpeterkesche@gmail.com>
2023-06-17 11:07:42 +0300

    kernelsu: re-implement properly now

--- fs/exec.c
+++ fs/exec.c
@@ -1712,8 +1712,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 	return ret;
 }
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1722,7 +1724,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
