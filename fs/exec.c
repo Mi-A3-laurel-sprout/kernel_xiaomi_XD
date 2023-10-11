6d2c17e1b2fdb9d4b4f996ed3981d03bae72fcb5
Skyblueborb <tomaszborbely0710@gmail.com>
2023-10-11 22:36:11 +0200

    [SQUASH] Revert KSU
    Revert "drivers: ksu: merge 1045 update"
    
    This reverts commit f83d40a81ad404a72313640b8675ab7502a905ef.
    
    Revert "drivers: kernelsu: hardcode ksu version for now"
    
    This reverts commit b3b71ef3b3005a17c2a882e811e02b00caa144cb.
    
    Revert "drivers: ksu: merge 1027 update"
    
    This reverts commit 418b575ccd5475082aa2e61a449bc3a3e6f07a26.
    
    Revert "drivers: ksu: merge 906 update"
    
    This reverts commit 1859397ad6f0b866ca93ddc393f7cb05920ef1ea.
    
    Revert "drivers: ksu: merge 873 update"
    
    This reverts commit cbebf33c05b72d4e053a86153147bdb6b20a4565.
    
    Revert "kernelsu: re-implement properly now"
    
    This reverts commit 8ff9b4d8eb39276fe01ad800e4bd4b2ae1fc7256.
    
    Revert "drivers: merge kernelsu update"
    
    This reverts commit 1c3295823162d5b4750efb862ad839e24f970ec6.
    
    Revert "kernelsu: drop KSU_GIT_VERSION check"
    
    This reverts commit 6c6a60131d267f618056720fd38dc91c0fc0c39e.
    
    Revert "kernelsu: update"
    
    This reverts commit 23e95f8f9cb6b641cf2694abab5052e370eb3086.
    
    Revert "drivers: import kernelsu"
    
    This reverts commit 8390187dd905bd6f99fa10db3150172f1091b349.
    
    Revert "fs: add kernelsu support"
    
    This reverts commit 0f61ef08143d1f86fa08347d9d35cf397415cd53.
    
    defconfig: remove KSU configs
    
    Revert "kernelsu: add SAFE MODE support"
    
    This reverts commit 194d6b811390849ab4e80cb85a31c71796b8842b.

--- fs/exec.c
+++ fs/exec.c
@@ -1712,10 +1712,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 	return ret;
 }
-#ifdef CONFIG_KSU
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1724,9 +1721,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
