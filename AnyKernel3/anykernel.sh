0efead3eb2864184bd5c9f82e10486e9065befc6
Magicxavi <rolfpeterkesche@gmail.com>
2024-01-22 14:50:12 +0200

    build: remove a11 support since we include KSU
    And add a14 to the supported list

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=laurel_sprout
 device.name2=MI A3
-supported.versions=11.0-13.0
+supported.versions=12.0-14.0
 supported.patchlevels=
 '; } # end properties
 
