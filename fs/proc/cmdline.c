684bfab2a4d1618c4c65c9589118d5a7ed8d1592
PowerX-NOT <pugalg100@gmail.com>
2025-02-02 10:19:29 +0530

    drivers: [PATCH]: Implement SUSFS v1.5.4
    https://gitlab.com/simonpunk/susfs4ksu/-/tree/kernel-4.14/kernel_patches?ref_type=heads

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -5,10 +5,20 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
+#ifdef KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 	seq_printf(m, "%s\n", new_command_line);
 	seq_putc(m, '\n');
 	return 0;
